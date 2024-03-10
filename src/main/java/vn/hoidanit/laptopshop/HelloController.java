2db5900ea784ae3334aa9655d6459ca6958067e7
QThai1704 <quangthai170402@gmail.com>
2024-03-10 16:29:49 +0700

    add lib and buil connect mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Nguyễn Quang Thái!";
+        return "Hello World with NQT!";
     }
 
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user access";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin access!";
+    }
 }
