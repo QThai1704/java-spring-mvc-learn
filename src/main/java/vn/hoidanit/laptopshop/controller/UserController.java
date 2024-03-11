272fcc82b6eb0b49f2629f8ba8aa62e42097938c
QThai1704 <quangthai170402@gmail.com>
2024-03-12 02:22:14 +0700

    Config url css and use jsp expression

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,32 +1,43 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
+    // DI: Dependency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    public String getHomePage() {
-        return userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage(Model mode) {
+        String text = this.userService.handleHello();
+        mode.addAttribute("text", text);
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// // DI: Dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return userService.handleHello();
+// }
+// }
